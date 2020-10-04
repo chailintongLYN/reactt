da45af6aac1083e576c1b0c9aa40963f232f6156
柴林彤 <44825541+chailintongLYN@users.noreply.github.com>
2020-10-04 17:34:43 +0800

    my react task

---
+++ precache-manifest.155770202d982be8bfd7fd5321af8d22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70592af788338017c0ae1b1f2e508ff1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98109381445896e0af0d",
+    "url": "/static/css/main.9e001d56.chunk.css"
+  },
+  {
+    "revision": "bc32e9450d867891c176",
+    "url": "/static/js/2.4a4c7e4d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4a4c7e4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98109381445896e0af0d",
+    "url": "/static/js/main.74256069.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
